bf9c7a18e7a83c63e47a1f198f837618194b9ec4
Adeel Ahmed Khan <adeelbarki@gmail.com>
2019-09-19 13:37:55 +0500

    updated kubectl & aws script

--- scripts/install-dockers.sh
+++ scripts/install-dockers.sh
@@ -7,9 +7,11 @@
 # sudo chown ubuntu:ubuntu <filename> (optional)
 # chmod +x install-dockers.sh
 
+
 # -----------------------------Install updates----------------------------------------
 apt update
 apt upgrade
+
 
 # -----------Install packages to allow apt to use a repository over HTTPS:-------------
 apt-get -y install \
@@ -18,8 +20,10 @@ apt-get -y install \
     curl \
     software-properties-common
 
+
 # ----------------------Add Docker’s official GPG key:---------------------------------
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
 
 # -----------------------Set up the stable repository---------------------------------
 add-apt-repository \
@@ -27,18 +31,22 @@ add-apt-repository \
    $(lsb_release -cs) \
    stable"
 
+
 # ------------------------Update the apt package index--------------------------------
 apt update
+
 
 # -------------------Install the latest version of Docker CE--------------------------
 apt-get -y install docker-ce
 
+
 # ----------------------------Check docker Version -----------------------------------
 # docker --version
+
 
 # -------------------Give permission to user Jenkins to run dockers-------------------
 # sudo usermod -a -G docker $USER
 sudo usermod -a -G docker jenkins
 # Restart Jenkins
-sudo systemctl restart jenkins
+systemctl restart jenkins
 
--- scripts/install-kubectl.sh
+++ scripts/install-kubectl.sh
@@ -1,5 +1,7 @@
-# Install kubectl
+#!/usr/bin/env bash
 
+
+# -------------------------------------Install kubectl-----------------------------------------------
 sudo apt update
 sudo apt upgrade
 curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl\
@@ -10,12 +12,9 @@ sudo mv ./kubectl /usr/local/bin/kubectl
 # Test if kubectl is working
 # kubectl version --short --client
 
-# -------------------------------------------------------------------------------------
 
-# Install AWS-iam-authenticator
+# --------------------------------Install AWS-iam-authenticator--------------------------------------
 curl -o aws-iam-authenticator https://amazon-eks.s3-us-west-2.amazonaws.com/1.14.6/2019-08-22/bin/linux/amd64/aws-iam-authenticator
-curl -o aws-iam-authenticator.sha256 https://amazon-eks.s3-us-west-2.amazonaws.com/1.14.6/2019-08-22/bin/linux/amd64/aws-iam-authenticator.sha256
-openssl sha1 -sha256 aws-iam-authenticator
 chmod +x ./aws-iam-authenticator
 sudo mv ./aws-iam-authenticator /usr/local/bin/aws-iam-authenticator
 # mkdir -p $HOME/bin && cp ./aws-iam-authenticator $HOME/bin/aws-iam-authenticator && export PATH=$HOME/bin:$PATH
@@ -24,55 +23,44 @@ sudo mv ./aws-iam-authenticator /usr/local/bin/aws-iam-authenticator
 # Test if aws-iam-authenticator is working
 # aws-iam-authenticator help
 
-# -------------------------------------------------------------------------------------------
 
-# Install aws-cli
+# -------------------------------------Install aws-cli----------------------------------------------
 
 # First check python / python3 and pip3 version 
 # python --version
 # python3 --version 
 # pip3 --version 
-# If not installed the install it. 
+# If not installed, to install it. 
+
 sudo apt -y install python3-pip
 curl -O https://bootstrap.pypa.io/get-pip.py
 python3 get-pip.py --user
-export PATH=~/.local/bin:$PATH (in jenkins user too)
-source ~/.bashrc
-# pip3 --version (check version)
+# export PATH=~/.local/bin:$PATH (in jenkins user too)
+# source ~/.bashrc
+# Now install aws-cli
+
+pip3 install awscli --upgrade --user
+# check pip3 version
+# pip3 --version
+# Move to local bin to get accessed through jenkins user
 sudo cp -r ./bin/* /usr/local/bin/
 
+# Check aws version from jenkins user
+# sudo su jenkins 
+# aws --version
 
-# Now install aws-cli
-# pip3 install awscli --upgrade --user
-# aws --version (check version)
+# Update aws credentials - IAM user programmetic acces - Amazon-admin access) 
+# aws configure
 
-# ------------------------------------------
 
-# aws configure (Update aws credentials - IAM user programmetic acces - Amazonadmin access) 
-# aws configure (in jenkins user)
-# Update kube-config file
-
+# ---------------------------------Update kube-config file------------------------------
 aws eks update-kubeconfig --name nginxcluster
 
 
-
-# ------------------------------------------
-
-# Install Docker (if not installed)
-# Look into install-dockers.sh file in order to install dockers
-
-
-# # Install Minikube
-# curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64\
-#  && chmod +x minikube && sudo mv minikube /usr/local/bin/
-
-#  # Run minikube
-#  minikube start --vm-driver=none
-
+# ----------------------------------------Optional---------------------------------------------------
 #  # Pull image from docker hub
 #  kubectl run capstone-clouddevops --image=adeelbarki/capstone-clouddevops:latest --port=80
 #  kubectl expose deployment capstone-clouddevops --type=LoadBalancer --port=80
 #  sudo kubectl get services
-#  minikube service capstone-clouddevops
 
 
