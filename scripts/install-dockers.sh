d3700ad3bd63e26d1fc4124f4c0c4021a95cbe38
Adeel Ahmed Khan <adeelbarki@gmail.com>
2019-09-19 13:26:52 +0500

    Updated docker installation script

--- scripts/install-dockers.sh
+++ scripts/install-dockers.sh
@@ -1,38 +1,44 @@
 #!/usr/bin/env bash
 
-# change permission on file
-# sudo chown ubuntu:group <filename>
-# chmod 700 <filename>
-# run sudo ./<filename> 
+# -------------------------------Pre-requiste----------------------------------------
+# sudo su -i (Make sure to login as root)
+# clone repo https://github.com/adeelbarki/capstone-clouddevops-docker-jenkins-aws-eks.git
+# change permission on file in script folder
+# sudo chown ubuntu:ubuntu <filename> (optional)
+# chmod +x install-dockers.sh
 
-# Install updates
+# -----------------------------Install updates----------------------------------------
 apt update
 apt upgrade
 
-# Install packages to allow apt to use a repository over HTTPS:
+# -----------Install packages to allow apt to use a repository over HTTPS:-------------
 apt-get -y install \
     apt-transport-https \
     ca-certificates \
     curl \
     software-properties-common
 
-# Add Docker’s official GPG key:
+# ----------------------Add Docker’s official GPG key:---------------------------------
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 
-# Set up the stable repository
+# -----------------------Set up the stable repository---------------------------------
 add-apt-repository \
    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
    $(lsb_release -cs) \
    stable"
 
-# Update the apt package index.
+# ------------------------Update the apt package index--------------------------------
 apt update
 
-# Install the latest version of Docker CE
+# -------------------Install the latest version of Docker CE--------------------------
 apt-get -y install docker-ce
 
-# Give permission to user
+# ----------------------------Check docker Version -----------------------------------
+# docker --version
+
+# -------------------Give permission to user Jenkins to run dockers-------------------
 # sudo usermod -a -G docker $USER
-# sudo usermod -a -G docker jenkins
-# sudo systemctl restart jenkins
+sudo usermod -a -G docker jenkins
+# Restart Jenkins
+sudo systemctl restart jenkins
 
