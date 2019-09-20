eff7aa10a0148f15664fd51161f86dea5dd8bff3
Adeel Ahmed Khan <adeelbarki@gmail.com>
2019-09-20 10:11:13 +0500

    code pylint test changes

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     html = f"<h3>Hello Udacity!</h3><p>This is Adeel Barki working remotely</p>\
-    <p>This is Blue/green deployment test for continuous integration</p>"
+        <p>This is Blue/green deployment test for continuous integration</p>"
     return html.format(format)
 
 
